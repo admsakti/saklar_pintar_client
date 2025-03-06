ff11a2038cf0c01192b3a80ac2f973865babc421
Sakti Bayu N <bayutabas87@gmail.com>
2025-03-06 15:55:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Saklar Pintar Client
+
+Aplikasi client menggunakan Flutter untuk perangkat saklar pintar berbasis Mesh Network
---
+++ android/app/src/main/kotlin/com/example/saklar_pintar_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.saklar_pintar_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
